cf622c80670306fbe95a5d99c084eaa8c359e08e
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2021-09-07 06:02:26 +0300

    feat: add typeorm-naming-strategies

--- product-service/src/db.ts
+++ product-service/src/db.ts
@@ -5,8 +5,10 @@ import {
   createConnection,
   getConnectionManager,
 } from "typeorm";
+import { SnakeNamingStrategy } from "typeorm-naming-strategies";
 
 import Product from "@resources/products/product.entity";
+import Stock from "@resources/stocks/stock.entity";
 
 const { PG_HOST, PG_PORT, PG_DATABASE, PG_USERNAME, PG_PASSWORD } = process.env;
 
@@ -38,7 +40,8 @@ export class DB {
         username: PG_USERNAME,
         database: PG_DATABASE,
         password: PG_PASSWORD,
-        entities: [Product],
+        namingStrategy: new SnakeNamingStrategy(),
+        entities: [Product, Stock],
       } as ConnectionOptions;
 
       connection = await createConnection(connectionOptions);
