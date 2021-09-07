a83a5fec2c7afbb13c9fbd6a7ad7da6dde9fdd04
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2021-09-07 03:11:51 +0300

    feat: db class implementation

---
+++ product-service/src/db.ts
@@ -0,0 +1,49 @@
+import {
+  Connection,
+  ConnectionManager,
+  ConnectionOptions,
+  createConnection,
+  getConnectionManager,
+} from "typeorm";
+
+import Product from "@resources/products/product.entity";
+
+const { PG_HOST, PG_PORT, PG_DATABASE, PG_USERNAME, PG_PASSWORD } = process.env;
+
+export class DB {
+  private connectionManager: ConnectionManager;
+
+  constructor() {
+    this.connectionManager = getConnectionManager();
+  }
+
+  public async getConnection(): Promise<Connection> {
+    const CONNECTION_NAME = `default`;
+
+    let connection: Connection;
+
+    if (this.connectionManager.has(CONNECTION_NAME)) {
+      connection = this.connectionManager.get(CONNECTION_NAME);
+
+      if (!connection.isConnected) {
+        connection = await connection.connect();
+      }
+    } else {
+      const connectionOptions = {
+        name: `default`,
+        type: `postgres`,
+        port: Number(PG_PORT),
+        synchronize: false,
+        host: PG_HOST,
+        username: PG_USERNAME,
+        database: PG_DATABASE,
+        password: PG_PASSWORD,
+        entities: [Product],
+      } as ConnectionOptions;
+
+      connection = await createConnection(connectionOptions);
+    }
+
+    return connection;
+  }
+}
