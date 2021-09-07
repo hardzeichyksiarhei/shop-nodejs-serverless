51c5e87dfd27660f5a6410b6e7d63089f7b9d49c
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2021-09-07 05:06:23 +0300

    feat: add callbackWaitsForEmptyEventLoop

--- product-service/src/functions/getProductById/handler.ts
+++ product-service/src/functions/getProductById/handler.ts
@@ -1,12 +1,19 @@
 import "source-map-support/register";
+import * as lambda from "aws-lambda";
 
 import { middyfy } from "@libs/lambda";
 
 import { ProductController } from "@resources/products/product.controller";
 const productController = ProductController.getInstance();
 
-const getProductById = async (event) => {
+const getProductById = async (
+  event: lambda.APIGatewayProxyEvent,
+  context: lambda.Context
+) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
   const productId = event.pathParameters.productId;
+
   const product = await productController.getById(productId);
   return product;
 };
--- product-service/src/functions/getProductsList/handler.ts
+++ product-service/src/functions/getProductsList/handler.ts
@@ -1,11 +1,17 @@
 import "source-map-support/register";
+import * as lambda from "aws-lambda";
 
 import { middyfy } from "@libs/lambda";
 
 import { ProductController } from "@resources/products/product.controller";
 const productController = ProductController.getInstance();
 
-const getProductList = async () => {
+const getProductList = async (
+  _event: lambda.APIGatewayProxyEvent,
+  context: lambda.Context
+) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
   const products = await productController.getAll();
   return products;
 };
