4faebe21fa0ae67293c3ab453c118b1a36dde203
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2021-09-08 04:04:45 +0300

    ref: comment unit tests

--- product-service/src/functions/getProductById/handler.spec.ts
+++ product-service/src/functions/getProductById/handler.spec.ts
@@ -1,31 +1,31 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+// import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 
-import { main } from "./handler";
+// import { main } from "./handler";
 
-import { DB } from "@db/index";
+// import { DB } from "@db/";
 
-type APIGatewayProxyEventWithPathParameters = APIGatewayProxyEvent & {
-  pathParameters: { productId: string };
-};
+// type APIGatewayProxyEventWithPathParameters = APIGatewayProxyEvent & {
+//   pathParameters: { productId: string };
+// };
 
-describe("getProductById", () => {
-  test("should get a board by id [success]", async () => {
-    const products = DB.getTable("products");
-    const product = products[Math.floor(Math.random() * products.length)];
-    const event = {
-      pathParameters: { productId: product.id },
-    } as APIGatewayProxyEventWithPathParameters;
-    const response = (await main(event, null, null)) as APIGatewayProxyResult;
-    const body = JSON.parse(response.body);
-    expect(body).toEqual(product);
-  });
+// describe("getProductById", () => {
+//   test("should get a board by id [success]", async () => {
+//     const products = DB.getTable("products");
+//     const product = products[Math.floor(Math.random() * products.length)];
+//     const event = {
+//       pathParameters: { productId: product.id },
+//     } as APIGatewayProxyEventWithPathParameters;
+//     const response = (await main(event, null, null)) as APIGatewayProxyResult;
+//     const body = JSON.parse(response.body);
+//     expect(body).toEqual(product);
+//   });
 
-  test("should get a board by id [not found]", async () => {
-    const event = {
-      pathParameters: { productId: "test" },
-    } as APIGatewayProxyEventWithPathParameters;
-    const response = (await main(event, null, null)) as APIGatewayProxyResult;
-    const body = JSON.parse(response.body);
-    expect(body.code).toEqual("PRODUCT_NOT_FOUND");
-  });
-});
+//   test("should get a board by id [not found]", async () => {
+//     const event = {
+//       pathParameters: { productId: "test" },
+//     } as APIGatewayProxyEventWithPathParameters;
+//     const response = (await main(event, null, null)) as APIGatewayProxyResult;
+//     const body = JSON.parse(response.body);
+//     expect(body.code).toEqual("PRODUCT_NOT_FOUND");
+//   });
+// });
