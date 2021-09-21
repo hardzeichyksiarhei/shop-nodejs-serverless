a6366730629b80304a37c87d432b76873a2f3e75
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2021-09-22 00:53:58 +0300

    feat: add REGION

--- import-service/src/config.ts
+++ import-service/src/config.ts
@@ -5,10 +5,14 @@ dotenv.config({
   path: path.join(__dirname, "../.env"),
 });
 
-const { IMPORT_BUCKET_NAME } = process.env;
+const { IMPORT_BUCKET_NAME, REGION } = process.env;
 
 const IMPORT_BUCKET_ARN = `arn:aws:s3:::${IMPORT_BUCKET_NAME}`;
 
-const config = { IMPORT_BUCKET_NAME, IMPORT_BUCKET_ARN };
+const config = {
+  IMPORT_BUCKET_NAME,
+  IMPORT_BUCKET_ARN,
+  REGION: REGION ?? "eu-west-1",
+};
 
 export default config;
