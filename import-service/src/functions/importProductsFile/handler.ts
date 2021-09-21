2fcc259c810d33b3f27a8593ece73bde74fb28e9
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2021-09-22 01:58:49 +0300

    feat: add s3 utils

--- import-service/src/functions/importProductsFile/handler.ts
+++ import-service/src/functions/importProductsFile/handler.ts
@@ -1,27 +1,28 @@
 import "source-map-support/register";
 import * as lambda from "aws-lambda";
-import AWS from "aws-sdk";
 
 import { middyfy } from "@libs/lambda";
 import { MessageUtil } from "@libs/message";
+import S3Utils from "../../s3";
 
 import config from "./../../config";
 
-const { IMPORT_BUCKET_NAME, REGION } = config;
+const { IMPORT_BUCKET_NAME } = config;
 
 const importProductsFile = async (event: lambda.APIGatewayProxyEvent) => {
   const filename = decodeURIComponent(event.queryStringParameters.name);
   console.log(`[filename]: ${filename}`);
 
   try {
-    const s3 = new AWS.S3({ region: REGION });
+    const s3u = S3Utils.getInstance();
+
     const params = {
       Bucket: IMPORT_BUCKET_NAME,
       Key: `uploaded/${filename}`,
       Expires: 60,
       ContentType: "text/csv",
     };
-    const signedUrl = await s3.getSignedUrlPromise("putObject", params);
+    const signedUrl = await s3u.s3.getSignedUrlPromise("putObject", params);
 
     return MessageUtil.success({ signedUrl });
   } catch (error) {
