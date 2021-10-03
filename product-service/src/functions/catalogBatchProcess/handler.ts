1ccee609439113ec896159f183e4a6c994a1ff8e
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2021-10-03 19:45:56 +0300

    feat: add catalogBatchProcess implementation

--- product-service/src/functions/catalogBatchProcess/handler.ts
+++ product-service/src/functions/catalogBatchProcess/handler.ts
@@ -11,6 +11,35 @@ const productController = ProductController.getInstance();
 
 const catalogBatchProcess = async (event: lambda.SQSEvent) => {
   const sns = new SNS();
+  const recordsData = event.Records.map((record) => JSON.parse(record.body));
+
+  try {
+    console.log(`[inserted products]: ${recordsData}`);
+    const highestPrice = Math.max(
+      ...recordsData.map(({ Price }) => Number(Price))
+    );
+
+    await sns
+      .publish({
+        Subject: `Products uploaded #${Date.now()}`,
+        Message: `Uploaded products:\n\n${JSON.stringify(
+          recordsData,
+          null,
+          2
+        )}`,
+        TopicArn: process.env.CREATE_PRODUCT_TOPIC_ARN,
+        MessageAttributes: {
+          highestPrice: {
+            DataType: "Number",
+            StringValue: String(highestPrice),
+          },
+        },
+      })
+      .promise();
+    console.log(`[email send]: Inserted ${recordsData.length} record(s)`);
+  } catch (e) {
+    console.error(e);
+  }
 };
 
 export const main = middyfy(catalogBatchProcess);
