3b0e6ad3a98036dab02743ae9f26d972caff3439
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2021-10-03 15:22:02 +0300

    feat: add serverless sns & sqs config

--- product-service/serverless.ts
+++ product-service/serverless.ts
@@ -3,12 +3,21 @@ import type { AWS } from "@serverless/typescript";
 import createProduct from "@functions/createProduct";
 import getProductsList from "@functions/getProductsList";
 import getProductById from "@functions/getProductById";
+import updateProductById from "@functions/updateProductById";
 import deleteProductById from "@functions/deleteProductById";
+import catalogBatchProcess from "@functions/catalogBatchProcess";
 
 import config from "src/config";
-import updateProductById from "@functions/updateProductById";
 
-const { PG_HOST, PG_PORT, PG_DATABASE, PG_USERNAME, PG_PASSWORD } = config;
+const {
+  PG_HOST,
+  PG_PORT,
+  PG_DATABASE,
+  PG_USERNAME,
+  PG_PASSWORD,
+  PRODUCTS_SUBSCRIPTION_EMAIL,
+  EXPENSIVE_PRODUCTS_SUBSCRIPTION_EMAIL,
+} = config;
 
 const serverlessConfiguration: AWS = {
   service: "product-service",
@@ -39,7 +48,26 @@ const serverlessConfiguration: AWS = {
       PG_DATABASE,
       PG_USERNAME,
       PG_PASSWORD,
+
+      PRODUCTS_SUBSCRIPTION_EMAIL,
+      EXPENSIVE_PRODUCTS_SUBSCRIPTION_EMAIL,
     },
+    iamRoleStatements: [
+      {
+        Effect: "Allow",
+        Action: "sqs:ReceiveMessage",
+        Resource: {
+          "Fn::GetAtt": ["CatalogItemsQueue", "Arn"],
+        },
+      },
+      {
+        Effect: "Allow",
+        Action: "sns:Publish",
+        Resource: {
+          Ref: "CreateProductTopic",
+        },
+      },
+    ],
   },
   // import the function via paths
   functions: {
@@ -48,6 +76,72 @@ const serverlessConfiguration: AWS = {
     getProductById,
     updateProductById,
     deleteProductById,
+    catalogBatchProcess,
+  },
+  resources: {
+    Resources: {
+      CatalogItemsQueue: {
+        Type: "AWS::SQS::Queue",
+        Properties: {
+          QueueName: "catalogItemsQueue",
+        },
+      },
+      CreateProductTopic: {
+        Type: "AWS::SNS::Topic",
+        Properties: {
+          TopicName: "createProductTopic",
+        },
+      },
+      CreateProductTopicSubscription: {
+        Type: "AWS::SNS::Subscription",
+        Properties: {
+          Protocol: "email",
+          Endpoint: PRODUCTS_SUBSCRIPTION_EMAIL,
+          TopicArn: {
+            Ref: "CreateProductTopic",
+          },
+        },
+      },
+      CreateExpensiveProductTopicSubscription: {
+        Type: "AWS::SNS::Subscription",
+        Properties: {
+          Protocol: "email",
+          Endpoint: EXPENSIVE_PRODUCTS_SUBSCRIPTION_EMAIL,
+          TopicArn: {
+            Ref: "CreateProductTopic",
+          },
+          FilterPolicy: {
+            highestPrice: [
+              {
+                numeric: [">=", 100],
+              },
+            ],
+          },
+        },
+      },
+    },
+    Outputs: {
+      QueueURL: {
+        Value: {
+          Ref: "CatalogItemsQueue",
+        },
+        Export: {
+          Name: {
+            "Fn::Sub": "${AWS::StackName}-CatalogItemsQueueUrl",
+          },
+        },
+      },
+      QueueARN: {
+        Value: {
+          "Fn::GetAtt": ["CatalogItemsQueue", "Arn"],
+        },
+        Export: {
+          Name: {
+            "Fn::Sub": "${AWS::StackName}-CatalogItemsQueueArn",
+          },
+        },
+      },
+    },
   },
 };
 
