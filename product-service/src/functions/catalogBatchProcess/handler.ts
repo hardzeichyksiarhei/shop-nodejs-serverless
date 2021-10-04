ee5f755d38e66a677fe1f26f93078b08db1a6bf3
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2021-10-04 15:01:04 +0300

    feat: function implementation

--- product-service/src/functions/catalogBatchProcess/handler.ts
+++ product-service/src/functions/catalogBatchProcess/handler.ts
@@ -4,17 +4,35 @@ import { SNS } from "aws-sdk";
 
 import { middyfy } from "@libs/lambda";
 
-import { ProductController } from "@resources/products/product.controller";
-import { CreateProductDto } from "@resources/products/dto/create-product.dto";
+import config from "../../config";
 
-const productController = ProductController.getInstance();
+// import { ProductController } from "@resources/products/product.controller";
+
+// const productController = ProductController.getInstance();
+
+import { DB } from "@db";
+import { Product } from "@resources/products/product.entity";
+
+const { REGION } = config;
 
 const catalogBatchProcess = async (event: lambda.SQSEvent) => {
-  const sns = new SNS();
+  const sns = new SNS({ region: REGION });
   const recordsData = event.Records.map((record) => JSON.parse(record.body));
 
+  // const db = new DB();
+  // const connection = await db.getConnection();
+
   try {
-    console.log(`[inserted products]: ${recordsData}`);
+    // for (const record of recordsData) {
+    //   await productController.create(record);
+    // }
+    // await connection
+    //   .createQueryBuilder()
+    //   .insert()
+    //   .into(Product)
+    //   .values(recordsData)
+    //   .execute();
+
     const highestPrice = Math.max(
       ...recordsData.map(({ price }) => Number(price))
     );
