39a8e25925bf5b3491f9b480dcac94915c5a3c73
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2021-10-04 16:15:45 +0300

    feat: insert products to database

--- product-service/src/functions/catalogBatchProcess/handler.ts
+++ product-service/src/functions/catalogBatchProcess/handler.ts
@@ -1,50 +1,64 @@
 import "source-map-support/register";
 import * as lambda from "aws-lambda";
 import { SNS } from "aws-sdk";
+import { Client, ClientConfig } from "pg";
 
 import { middyfy } from "@libs/lambda";
 
 import config from "../../config";
+import { MessageUtil } from "@libs/message";
 
-// import { ProductController } from "@resources/products/product.controller";
+const { PG_USERNAME, PG_HOST, PG_DATABASE, PG_PASSWORD, PG_PORT, REGION } =
+  config;
 
-// const productController = ProductController.getInstance();
-
-import { DB } from "@db";
-import { Product } from "@resources/products/product.entity";
-
-const { REGION } = config;
+const dbOptions: ClientConfig = {
+  user: PG_USERNAME,
+  host: PG_HOST,
+  database: PG_DATABASE,
+  password: PG_PASSWORD,
+  port: Number(PG_PORT),
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  connectionTimeoutMillis: 5000,
+};
 
 const catalogBatchProcess = async (event: lambda.SQSEvent) => {
   const sns = new SNS({ region: REGION });
-  const recordsData = event.Records.map((record) => JSON.parse(record.body));
+  const records = event.Records.map((record) => JSON.parse(record.body));
 
-  // const db = new DB();
-  // const connection = await db.getConnection();
+  const dbClient = new Client(dbOptions);
+  await dbClient.connect();
+
+  const ids = [];
 
   try {
-    // for (const record of recordsData) {
-    //   await productController.create(record);
-    // }
-    // await connection
-    //   .createQueryBuilder()
-    //   .insert()
-    //   .into(Product)
-    //   .values(recordsData)
-    //   .execute();
+    for (const { title, description, price, count } of records) {
+      await dbClient.query("BEGIN");
 
-    const highestPrice = Math.max(
-      ...recordsData.map(({ price }) => Number(price))
-    );
+      const { rows } = await dbClient.query(
+        "insert into products(title, description, price) values ($1, $2, $3) returning id",
+        [title, description, price]
+      );
+
+      const { id } = rows[0];
+
+      await dbClient.query(
+        "insert into stocks(product_id, count) values ($1, $2)",
+        [id, count]
+      );
+
+      await dbClient.query("COMMIT");
+
+      ids.push(id);
+    }
+
+    const highestPrice = Math.max(...records.map(({ price }) => Number(price)));
 
     await sns
       .publish({
         Subject: `Products uploaded #${Date.now()}`,
-        Message: `Uploaded products:\n\n${JSON.stringify(
-          recordsData,
-          null,
-          2
-        )}`,
+        Message: `Uploaded products:\n\n${JSON.stringify(records, null, 2)}`,
         TopicArn: process.env.CREATE_PRODUCT_TOPIC_ARN,
         MessageAttributes: {
           highestPrice: {
@@ -54,9 +68,13 @@ const catalogBatchProcess = async (event: lambda.SQSEvent) => {
         },
       })
       .promise();
-    console.log(`[email send]: Inserted ${recordsData.length} record(s)`);
+
+    return MessageUtil.success({ ids });
   } catch (e) {
-    console.error(e);
+    await dbClient.query("ROLLBACK");
+    return MessageUtil.error(e);
+  } finally {
+    dbClient.end();
   }
 };
 
