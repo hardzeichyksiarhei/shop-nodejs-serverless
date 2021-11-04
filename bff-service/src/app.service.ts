37525435fe9e99bd0629d5417878bfd6d59c10bb
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2021-11-04 20:35:44 +0300

    feat: add  request service

--- bff-service/src/app.service.ts
+++ bff-service/src/app.service.ts
@@ -1,7 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable()
 export class AppService {
   constructor(private httpService: HttpService) {}
+  
+  async request({ method, url, body }) {
+    const data = Object.keys(body || {}).length > 0 && { data: body }
+    const requestConfig = { method, url, ...data };
+    const response$ = this.httpService.request(requestConfig);
+    return lastValueFrom(response$);
+  }
 }
