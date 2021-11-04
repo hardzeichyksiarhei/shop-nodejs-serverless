e6193c90ae1d84955a2f6e944ccdcc87aecf4daf
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2021-11-04 22:31:49 +0300

    feat: add root controller

--- bff-service/src/app.controller.ts
+++ bff-service/src/app.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, All, Req, Res, HttpStatus, HttpException } from '@nestjs/common';
+import { Response, Request } from 'express';
+
 import { AppService } from './app.service';
+
+import { API_URL } from './environments';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @All('*')
+  async root(@Req() req: Request) {
+    const { originalUrl } = req;
+    const [_, recipient] = originalUrl.split('/');
+    const recipientUrl = API_URL[recipient.toLowerCase()];
+
+    if (recipientUrl) return recipientUrl
+    
+    throw new HttpException('Cannot process request', HttpStatus.BAD_GATEWAY);
   }
 }
