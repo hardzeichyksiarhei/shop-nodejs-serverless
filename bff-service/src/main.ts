a3f67841c4519917ce21a17259ba1bda76ab92c8
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2021-11-04 22:34:31 +0300

    feat: log after run app

--- bff-service/src/main.ts
+++ bff-service/src/main.ts
@@ -8,6 +8,7 @@ import { PORT } from './environments';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors();
-  await app.listen(PORT);
+  await app.listen(PORT, '0.0.0.0');
+  console.log(`🚀 Server is listening on port ${PORT}`);
 }
 bootstrap();
