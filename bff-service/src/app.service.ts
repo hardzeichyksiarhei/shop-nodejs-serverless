cf007b40996904c7cd9d45f73900c867dd746371
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2021-11-04 20:13:56 +0300

    feat: inject http module

--- bff-service/src/app.service.ts
+++ bff-service/src/app.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+  constructor(private httpService: HttpService) {}
 }
